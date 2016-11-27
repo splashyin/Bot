19ff1c1eced6a3c7273c660105fb9f7abc9ba633
splashyin <yin.hao@rocketmail.com>
2016-11-27 18:07:29 +1300

    exchange rate update

--- Bot ApplicationBank/Bot ApplicationBank/Controllers/MessagesController.cs
+++ Bot ApplicationBank/Bot ApplicationBank/Controllers/MessagesController.cs
@@ -7,6 +7,11 @@ using System.Web.Http;
 using System.Web.Http.Description;
 using Microsoft.Bot.Connector;
 using Newtonsoft.Json;
+using Bot_ApplicationBank.Model;
+using System.Linq;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Collections;
 
 namespace Bot_ApplicationBank
 {
@@ -23,17 +28,67 @@ namespace Bot_ApplicationBank
             {
                 ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
 
+                ExchangeObject.RootObject rootObject;
+                 
 
+                HttpClient client = new HttpClient();
 
-                // calculate something for us to return
-                int length = (activity.Text ?? string.Empty).Length;
-                DateTime time = activity.Timestamp.Value;
-                // return our reply to the user
-                Activity reply = activity.CreateReply($"You sent {activity.Text} at {time}");
+              
+                Activity reply;
 
+                string temp = "";
 
+                var dictionary = new Dictionary<string, object> { };
 
-                await connector.Conversations.ReplyToActivityAsync(reply);
+                if (activity.Text.ToLower().Contains("show") & activity.Text.ToLower().Contains("base"))
+                {
+                    string basicRate = await client.GetStringAsync(new Uri("http://api.fixer.io/latest"));
+                    rootObject = JsonConvert.DeserializeObject<ExchangeObject.RootObject>(basicRate);
+
+                    Type myType = rootObject.rates.GetType();
+                    IList<PropertyInfo> props = new List<PropertyInfo>(myType.GetProperties());
+
+                    List<string> rateList = new List<string>();
+
+                    foreach (PropertyInfo prop in props)
+                    {
+                        object propValue = prop.GetValue(rootObject.rates, null);
+                        dictionary.Add(prop.Name, propValue);
+                    }
+                    
+                    foreach (KeyValuePair<string, object> kvp in dictionary)
+                    {
+                        temp = string.Format("{0}, {1}", kvp.Key, kvp.Value);
+                        rateList.Add(temp);
+                    }
+                    reply = activity.CreateReply($"At the date: {rootObject.date}, base currency is {rootObject.@base}, rate is\r\n{String.Join(Environment.NewLine +"", rateList)}");
+                    await connector.Conversations.ReplyToActivityAsync(reply);
+                }
+
+                else if(activity.Text.Length == 3)
+                {
+                    string specifiedRate = await client.GetStringAsync(new Uri("http://api.fixer.io/latest?base=" + activity.Text));
+                    rootObject = JsonConvert.DeserializeObject<ExchangeObject.RootObject>(specifiedRate);
+                    Type myType = rootObject.rates.GetType();
+                    IList<PropertyInfo> props = new List<PropertyInfo>(myType.GetProperties());
+
+                    List<string> rateList = new List<string>();
+                   
+                    foreach (PropertyInfo prop in props)
+                    {
+                        object propValue = prop.GetValue(rootObject.rates, null);
+                        dictionary.Add(prop.Name, propValue);
+                    }
+
+                    foreach (KeyValuePair<string, object> kvp in dictionary)
+                    {
+                        temp = string.Format("{0}, {1}", kvp.Key, kvp.Value);
+                        rateList.Add(temp);
+                    }
+                    reply = activity.CreateReply($"At the date: {rootObject.date}, base currency is {rootObject.@base}, rate is\r\n{String.Join(Environment.NewLine + "", rateList)}");
+                    await connector.Conversations.ReplyToActivityAsync(reply);
+                }
+
             }
             else
             {
@@ -62,7 +117,7 @@ namespace Bot_ApplicationBank
                 // Activity.From + Activity.Action represent what happened
             }
             else if (message.Type == ActivityTypes.Typing)
-            {
+            { 
                 // Handle knowing tha the user is typing
             }
             else if (message.Type == ActivityTypes.Ping)
