d8b042d393fe62c6af777ffea24cb5e065b1e94c
splashyin <yin.hao@rocketmail.com>
2016-11-28 13:36:30 +1300

    databse

---
+++ Bot ApplicationBank/Bot ApplicationBank/AzureManager.cs
@@ -0,0 +1,51 @@
+﻿using Bot_ApplicationBank.DataModels;
+using Microsoft.WindowsAzure.MobileServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Bot_ApplicationBank
+{
+    public class AzureManager
+    {
+
+        private static AzureManager instance;
+        private MobileServiceClient client;
+        private IMobileServiceTable<Timeline> timelinetable;
+        private AzureManager()
+        {
+            this.client = new MobileServiceClient("http://bankbottimeline.azurewebsites.net");
+            this.timelinetable = this.client.GetTable<Timeline>();
+        }
+
+        public MobileServiceClient AzureClient
+        {
+            get { return client; }
+        }
+
+        public static AzureManager AzureManagerInstance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new AzureManager();
+                }
+
+                return instance;
+            }
+        }
+
+        public async Task AddTimeLine(Timeline timeline)
+        {
+            await this.timelinetable.InsertAsync(timeline);
+        }
+
+        public async Task<List<Timeline>> GetTimelines()
+        {
+            return await this.timelinetable.ToListAsync();
+        }
+    }
+}
---
+++ Bot ApplicationBank/Bot ApplicationBank/DataModels/TimeLine.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bot_ApplicationBank.DataModels
+{
+    public class Timeline
+    {
+        [JsonProperty(PropertyName = "Id")]
+        public string ID { get; set; }
+
+        [JsonProperty(PropertyName = "name")]
+        public double Name { get; set; }
+
+        
+
+
+    }
+}
